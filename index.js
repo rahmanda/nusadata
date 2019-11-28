354e82cd84dab36e5a82a1bed590ed4e347c117e
Rahmanda Wibowo <rahmandawibowo@gmail.com>
2019-11-29 01:21:34 +0700

    feat: add dengue visualization

--- index.js
+++ index.js
@@ -3,9 +3,8 @@ const express = require('express');
 const server = express();
 
 const port = 3000;
-const assetPath = '/ast';
 const publicPath = path.join(__dirname, 'public');
 
-server.use(assetPath, express.static(publicPath));
+server.use(express.static(publicPath));
 
-server.listen(port, () => console.log(`Server is running on port ${port}!`);
+server.listen(port, () => console.log(`Server is running on port ${port}!`));
